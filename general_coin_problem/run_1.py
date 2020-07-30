23d11bad49b3b3c62268a6b0bfc3c0a615816cb8
RyL <31062500+RyL20200506@users.noreply.github.com>
2020-07-30 11:47:08 +0800

    更新似然函数，但是在选取参数的时候还有些问题

--- general_coin_problem/run_1.py
+++ general_coin_problem/run_1.py
@@ -26,30 +26,41 @@ print(S_0)
 # -------------
 
 
-def p_i(result, P_H_H_A, P_H_H_B, P_H_T_A, P_H_T_B, P_A):  # 输入实验结果，输出该实验结果的似然函数
+def p_i(P_H_H_A, P_H_H_B, P_H_T_A, P_H_T_B, P_A, P_H, result=[1, 1, 0, 0, 1]):  # 输入实验结果，输出该实验结果的似然函数
     paras = {
-        (1, 1): [P_H_H_A, P_A_H_A], (1, 0): [P_H_H_B, P_A_H_B],
-        (0, 1): [P_H_T_A, P_A_T_A], (0, 0): [P_H_T_B, P_A_T_B]
-    }
-    if result == (1, 1):
-        P = paras[result]
-        # 累乘
-        L = P_H_H_A*P_A + P_H_H_B*(1 - P_A)
-        L = paras[(1, 1)][0]*P_A + paras[(1, 0)]*(1 - P_A)
-    if result == (0, 1):
-        L = P_H_T_A*P_A + P_H_T_B*(1 - P_A)
-        L = paras[(0, 1)][0]*P_A + paras[(0, 0)]*(1 - P_A)
-    if result == (1, 1, 1):
-        L_1_1 = P_H_H_A*P_A + P_H_H_B*(1 - P_A)
-        H_1_1
-        L =
+            (1, 1): [P_H_H_A, P_A_H_A], (1, 0): [P_H_H_B, P_A_H_B],
+            (0, 1): [P_H_T_A, P_A_T_A], (0, 0): [P_H_T_B, P_A_T_B]
+            }
 
-        return L
+    Matrices_p = []
+    '''
+    P(S_01=H), P(S_00=A)
+    P(S_11=H), P(S_10=A)
+    P(S_21=H), P(S_20=A)
+    P(S_31=H), P(S_30=A)
+    ...
+    '''
+    p_0_H = P_H
+    p_0_A = P_A
+    Matrices_p.append([p_0_H, p_0_A])
 
-    def likelihood_function(P_H_H_A, P_H_H_B, P_H_T_A, P_H_T_B, P_A):
-        return (P_H_H**H_H) * (P_H_T**H_T) * (1-P_H_H)**T_H * (1-P_H_T)**T_T
+    for i in range(1, len(result)):
+        print(i)
+        '''
+        p_1_H = P_H_H_A * Matrices_p[0][1] + P_H_H_B * (1 - Matrices_p[0][1])
+        p_1_A = P_A_H_A * Matrices_p[0][1] + P_A_H_B * (1 - Matrices_p[0][1])
+        '''
+        Matrices_p.append([P_H_H_A * Matrices_p[i][1] + P_H_H_B * (1 - Matrices_p[i][1]),
+                           P_A_H_A * Matrices_p[i][1] + P_A_H_B * (1 - Matrices_p[i][1])])
+        # todo 这里只考虑了上一次是H的情况，如果上次的结果不是H呢？
 
-    return
+    L = 1
+    for i in range(len(result)):
+        if result[i] == 1:
+            L *= Matrices_p[i][0]
+        elif result[i] == 0:
+            L *= 1 - Matrices_p[i][0]
+    return L
 
 p_i(1)
 
