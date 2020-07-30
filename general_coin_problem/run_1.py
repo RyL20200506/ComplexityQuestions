9fe5a432d476e2abedf280e13d89cd24c02c2587
RyL <31062500+RyL20200506@users.noreply.github.com>
2020-07-30 17:23:04 +0800

    修复似然函数，需要测试

--- general_coin_problem/run_1.py
+++ general_coin_problem/run_1.py
@@ -22,17 +22,16 @@ for i in range(50):
     S_1.append(int(np.random.binomial(1, P_H)))
     S_0.append(int(np.random.binomial(1, P_A)))
 print(S_0)
-
 # -------------
 
 
-def p_i(P_H_H_A, P_H_H_B, P_H_T_A, P_H_T_B, P_A, P_H, result=[1, 1, 0, 0, 1]):  # 输入实验结果，输出该实验结果的似然函数
-    paras = {
-            (1, 1): [P_H_H_A, P_A_H_A], (1, 0): [P_H_H_B, P_A_H_B],
-            (0, 1): [P_H_T_A, P_A_T_A], (0, 0): [P_H_T_B, P_A_T_B]
-            }
+def likelihood(P_H_H_A, P_H_H_B, P_H_T_A, P_H_T_B, P_A, P_H, result=[1, 1, 0, 0, 1]):  # 输入实验结果，输出该实验结果的似然函数
+    # parameters & theta
+    paras = {(1, 1): [P_H_H_A, P_A_H_A], (1, 0): [P_H_H_B, P_A_H_B],
+            (0, 1): [P_H_T_A, P_A_T_A], (0, 0): [P_H_T_B, P_A_T_B]}
 
-    Matrices_p = []
+    # history probability function
+    Matrices_p = [[P_H, P_A]]
     '''
     P(S_01=H), P(S_00=A)
     P(S_11=H), P(S_10=A)
@@ -40,19 +39,22 @@ def p_i(P_H_H_A, P_H_H_B, P_H_T_A, P_H_T_B, P_A, P_H, result=[1, 1, 0, 0, 1]):  
     P(S_31=H), P(S_30=A)
     ...
     '''
-    p_0_H = P_H
-    p_0_A = P_A
-    Matrices_p.append([p_0_H, p_0_A])
 
     for i in range(1, len(result)):
         print(i)
         '''
-        p_1_H = P_H_H_A * Matrices_p[0][1] + P_H_H_B * (1 - Matrices_p[0][1])
-        p_1_A = P_A_H_A * Matrices_p[0][1] + P_A_H_B * (1 - Matrices_p[0][1])
+        The probabiliry of (H,A) at the second time, 
+        p_1_H = P(H|last_coin, A) * P(A) + P(H|last_coin, B) * (1-P(A))
+        p_1_A = P(A|last_coin, A) * P(A) + P(A|last_coin, B) * (1-P(A))
         '''
-        Matrices_p.append([P_H_H_A * Matrices_p[i][1] + P_H_H_B * (1 - Matrices_p[i][1]),
-                           P_A_H_A * Matrices_p[i][1] + P_A_H_B * (1 - Matrices_p[i][1])])
-        # todo 这里只考虑了上一次是H的情况，如果上次的结果不是H呢？
+        # Matrices_p.append([P_H_H_A * Matrices_p[i-1][1] + P_H_H_B * (1 - Matrices_p[i-1][1]),
+        #                    P_A_H_A * Matrices_p[i-1][1] + P_A_H_B * (1 - Matrices_p[i-1][1])])
+        # Matrices_p.append([paras[(1, 1)][0] * Matrices_p[i-1][1] + paras[(1, 0)][0] * (1 - Matrices_p[i-1][1]),
+        #                    paras[(1, 1)][1] * Matrices_p[i-1][1] + P_A_H_B[(1, 0)][1] * (1 - Matrices_p[i-1][1])])
+        last_coin_result = result[i-1]
+        last_probability = Matrices_p[i-1]
+        Matrices_p.append([paras[(last_coin_result, 1)][0] * last_probability[1] + paras[(last_coin_result, 0)][0] * (1 - last_probability[1]),
+                           paras[(last_coin_result, 1)][1] * last_probability[1] + paras[(last_coin_result, 0)][1] * (1 - last_probability[1])])
 
     L = 1
     for i in range(len(result)):
@@ -60,9 +62,11 @@ def p_i(P_H_H_A, P_H_H_B, P_H_T_A, P_H_T_B, P_A, P_H, result=[1, 1, 0, 0, 1]):  
             L *= Matrices_p[i][0]
         elif result[i] == 0:
             L *= 1 - Matrices_p[i][0]
-    return L
+    return L, Matrices_p
 
-p_i(1)
+
+test = likelihood(P_H_H_A=0.3, P_H_H_B=0.7, P_H_T_A=0.2, P_H_T_B=0.9, P_A=1, P_H=1, result=[1, 1, 0, 0, 1])
+
 
 
 
