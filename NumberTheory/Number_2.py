ca998e8148f4fb8fbbabf61ab057115f7381cb1d
RyL20200506 <775713638@qq.com>
2020-08-28 22:33:43 +0800

    tb

--- NumberTheory/Number_2.py
+++ NumberTheory/Number_2.py
@@ -74,21 +74,24 @@ plt.plot(list)
 plt.title('1%10+2%10+(m-1)%10')
 
 
-# 11^m的千分位
+# 11^m的千分位中的百分位部分
 from scipy.special import comb, perm
 c_s = []
 c1_s = []
+c2_s = []
 for m in range(1, 50):
     a = int(comb(m, 2))
     b = ( (comb(m, 1) - comb(m, 1)%10)/10 + comb(m, 2) ) % 10
-    # a1能够消除差距而构造的
-    a1 = (int(m/10) % 10 + a % 10) % 10  # q + comb(m,2)
+    # a1: 为了消除差距而构造的
+    a1 = (int(m/10) % 10 + comb(m, 2) % 10) % 10  # q + comb(m,2)
 
     # 对比个位数差异
     c = (a%10 - b%10)%-10
     c1 = (a1%10 - b%10)%-10
+    c2 = -int(m/10)%-10
 
     c1_s.append(c1)
+    c2_s.append(c2)
     c_s.append(c)
     print(m, a, b, c)
     print(a1, b)
@@ -97,8 +100,34 @@ for m in range(1, 50):
 import matplotlib.pyplot as plt
 plt.clf()
 plt.plot(c_s)
-plt.plot(c1_s)
+# plt.plot(c1_s)
+plt.plot(c2_s)
 
+# 11^m的千分位中的千分位
+list = []
+str_list = []
+for m in range(4, 100):
+    # r = m*(m-1)/2 % 10
+    q = int(m / 10) % 10
+    x0 = 1 # 1
+    x1 = ((x0 - x0 % 10) / 10) + comb(m, 1)  # 10
+    x2 = ((x1 - x1 % 10) / 10) + comb(m, 2)  # 100
+    x3 = ((x2 - x2 % 10) / 10) + comb(m, 3)  # 1000
+    x4 = ((x3 - x3 % 10) / 10) + comb(m, 4)  # 10000 # 记得改comb(m, 4)中的4
+
+    r = x4  # input: 以谁为展示结果
+    n = -5  # input: 对应第几项，-1为个位数
+    list.append(r%10)
+    str_list.append(int(str(11 ** m)[n]))
+    print(m, str(int(r)%10), str(11 ** m)[n])
+import matplotlib.pyplot as plt
+plt.clf()
+plt.plot(list)  # 通项公式数据
+plt.plot(str_list)  # 真实结果数据
+
+for m in range(4,20):
+    r = 11**m
+    print(r, str(r)[-4])
 
 
 
